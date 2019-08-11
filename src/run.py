a3624f8f36458afa4923c353c868fcea8ef15e4f
Luke Texon <luk.tex37@gmail.com>
2019-08-11 14:48:06 +0100

    Integrated Sentry

--- src/run.py
+++ src/run.py
@@ -5,6 +5,7 @@ from bot import Regime
 roles_path = "data/roles.json"
 
 if __name__ == "__main__":
+    sentry_sdk.init(dsn=f"https://{os.environ['sentry-key']}@sentry.io/{os.environ['sentry-project']}")
     with open(roles_path) as roles_json:
         roles = json.load(roles_json)
 
